90e38332cc33e6d19952c1e4acdd092e9a28413e
fjarado <fj_arado@hotmail.com>
2019-08-20 15:44:02 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   imports: [
@@ -20,7 +21,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   bootstrap: [
     AppComponent
