6e2778915a30e1a5ea7186cd1e5abbec4cd812d6
fjarado <fj_arado@hotmail.com>
2019-08-22 13:42:29 -0400

    1 file modified

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.componenr.css']
+  styleUrls: ['./shopping-list.component.css']
 })
 
 export class ShoppingListComponent {
