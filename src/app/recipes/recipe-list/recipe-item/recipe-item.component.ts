6b38212d02f405203b550bcb34976bd9eb8b9054
fjarado <fj_arado@hotmail.com>
2019-08-22 13:38:52 -0400

    1 file modified

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html'
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
 })
 
 export class RecipeItemComponent {
