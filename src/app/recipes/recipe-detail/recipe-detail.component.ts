c9e173d5536411a95f4e14f297b4d76c70e554de
fjarado <fj_arado@hotmail.com>
2019-08-22 13:39:53 -0400

    1 file modified

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html'
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['.recipe-detail.component.css']
 })
 
 export class RecipeDetailComponent {
