b3eed3d10f6ee54a33e7ed5e4ac72f035c3d4859
fjarado <fj_arado@hotmail.com>
2019-08-22 13:37:36 -0400

    1 file modified

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html'
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
 })
 
 export class ShoppingEditComponent {
