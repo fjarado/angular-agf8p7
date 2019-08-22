0bfa17468ce48f32e65af84e0152985db4cb734b
fjarado <fj_arado@hotmail.com>
2019-08-22 15:07:38 -0400

    1 file modified

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,6 +10,6 @@ import { Recipe } from '../recipe.model';
 
 export class RecipeListComponent {
   recipes: Recipe = [
-    new Recipe('A Test Recipe', 'This is simple a test')
+    new Recipe('A Test Recipe', 'This is simple a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 }
