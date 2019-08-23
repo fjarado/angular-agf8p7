e3276ae586271b6783a95f6e2bba2eb94fa90bcb
fjarado <fj_arado@hotmail.com>
2019-08-23 14:34:31 -0400

    1 file modified

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -9,5 +9,8 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 
 export class ShoppingListComponent {
-  ingredients: Ingredient[] = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 }
