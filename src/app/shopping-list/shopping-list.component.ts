2c73e2edc49b753796c9d6457f179f7c2fef2241
fjarado <fj_arado@hotmail.com>
2020-04-27 22:25:04 -0400

    1 file modified

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -10,6 +10,14 @@ import { Ingredient } from "../shared/ingredient.model";
 export class ShoppingListComponent {
   ingredients: Ingredient[] = [
     new Ingredient("Apples", 5),
-    new Ingredient("Tomatoes", 10)
+    new Ingredient("Tomatoes", 10),
+    new Ingredient("Oranges", 7),
+    new Ingredient("Grapes", 12),
+    new Ingredient("Bananas", 20),
+    new Ingredient("Pears", 23),
+    new Ingredient("Cereals", 27),
+    new Ingredient("Potatoes", 30),
+    new Ingredient("Vegetables", 15),
+    new Ingredient("Strawberries", 37)
   ];
 }
